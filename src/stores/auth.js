983685d3a1e40026f458dfdc6adf3e58a51e2f18
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-15 12:44:25 +0200

    chore: remove dist from git and add to gitignore

---
+++ src/stores/auth.js
@@ -0,0 +1,16 @@
+// src/stores/auth.js
+import { defineStore } from "pinia";
+import { ref } from 'vue';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { auth, db } from '@/firebase';
+
+export const useAuthStore = defineStore(auth, () => {
+    const login = async (email, password) => {
+        await signInWithEmailAndPassword(auth, email, password)
+    };
+
+    return {
+        login
+    };
+})
