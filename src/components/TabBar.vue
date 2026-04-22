ca1f99bea06c8b3650637b3ac49fd2a9eba3d6ee
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-22 13:01:21 +0200

    feat: add TabBar icons

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-
+import { Home, TrendingUp, FileText, MessageSquareMore } from '@lucide/vue';
 
 const tabs = [
-    { label: 'Hjem', route: '/customer' },
-    { label: 'Byggeforløb', route: '/customer/process' },
-    { label: 'Dokumenter', route: '/customer/documents' },
-    { label: 'Chat', route: '/customer/chat-options'}
+    { label: 'Hjem', route: '/customer', icon: Home },
+    { label: 'Byggeforløb', route: '/customer/process', icon: TrendingUp },
+    { label: 'Dokumenter', route: '/customer/documents', icon: FileText },
+    { label: 'Chat', route: '/customer/chat-options', icon: MessageSquareMore }
 ];
 </script>
 
 <template>
     <nav> 
         <RouterLink v-for="tab in tabs" :key="tab.label" :to="tab.route">
+            <component :is="tab.icon"/>
             {{ tab.label }}
         </RouterLink>
     </nav>
