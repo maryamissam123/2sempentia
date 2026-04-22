30061c62167cba39af970af082a6278e7cde9704
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-22 12:19:43 +0200

    feat: add static tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import TabBar from '@/components/TabBar.vue';
+
+</script>
 
 <template>
   <RouterView />
+  <TabBar />
 </template>
 
 <style scoped></style>
--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,6 +1,17 @@
 <script setup>
+
+const tabs = [
+    { label: 'Hjem' },
+    { label: 'Byggeforløb' },
+    { label: 'Dokumenter' },
+    { label: 'Chat'}
+];
 </script>
 
 <template>
-    <div></div> 
+    <nav> 
+        <button v-for="tab in tabs" :key="tab.label">
+            {{ tab.label }}
+        </button>
+    </nav>
 </template>
