a787641ca16ffc396eb8a9931e10803774ae5165
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-22 13:30:15 +0200

    feat: split TabBar by role

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -2,12 +2,21 @@
 import { RouterLink } from 'vue-router';
 import { Home, TrendingUp, FileText, MessageSquareMore } from '@lucide/vue';
 
-const tabs = [
+const customerTabs = [
     { label: 'Hjem', route: '/customer', icon: Home },
     { label: 'Byggeforløb', route: '/customer/process', icon: TrendingUp },
     { label: 'Dokumenter', route: '/customer/documents', icon: FileText },
     { label: 'Chat', route: '/customer/chat-options', icon: MessageSquareMore }
 ];
+
+const managerTabs = [
+    { label: 'Hjem', route: '/manager', icon: Home },
+    { label: 'Byggeforløb', route: '/manager/process', icon: TrendingUp },
+    { label: 'Dokumenter', route: '/manager/documents', icon: FileText },
+    { label: 'Chat', route: '/manager/chat-options', icon: MessageSquareMore }
+];
+
+const tabs = managerTabs
 </script>
 
 <template>
