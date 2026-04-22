e175a7c938e61a72fff30181830635bc8b15defd
maryamissam123 <maryamissam123@gmail.com>
2026-04-22 10:38:46 +0200

    fix: move firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCU939UMb5FnzHBOps1AcbgW3Qhm0lTy0",
-  authDomain: "pentia-webdev.firebaseapp.com",
-  projectId: "pentia-webdev",
-  storageBucket: "pentia-webdev.firebasestorage.app",
-  messagingSenderId: "159534191107",
-  appId: "1:159534191107:web:1b26e9cba803f625e4b436"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
