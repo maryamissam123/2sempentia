fc79bd761785746f01ead7da7eadf69c80017afd
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-24 16:42:58 +0200

    feat: add fetchProject function to project store

--- src/stores/project.js
+++ src/stores/project.js
@@ -1,16 +1,21 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { db } from '@/firebase'
 
 export const useProjectStore = defineStore('project', () => {
 	const projects = ref([]);
+	const project = ref(null)
 
   async function fetchProjects() {
 		const snap = await getDocs(collection(db, 'projects'));
 		projects.value = snap.docs.map(d => ({ id: d.id, ...d.data() }));
-
   };
 
-  return { projects, fetchProjects };
+	async function fetchProject() {
+		const snap = await getDocs(doc(db, 'projects', id));
+		project.value = { id: snap.id, ...snap.data() }
+	}
+
+  return { projects, project, fetchProjects, fetchProject };
 });
