ac40985520c8bc4f4492ea6c1d3d6e7eb11bc798
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-24 15:24:16 +0200

    feat: add meta titles and layouts to routes

--- src/router/index.js
+++ src/router/index.js
@@ -16,38 +16,43 @@ const router = createRouter({
         {
           path: '',
           name: 'customer-dashboard',
-          meta: { title: 'Kunde Dashboard' },
+          meta: { title: 'Mit Hus' },
           component: () => import('@/views/customer/DashboardView.vue'),
         },
         {
           path: 'process',
           name: 'customer-process',
-          meta: { title: 'Byggeforløb', layout: 'detail' },
+          meta: { title: 'Byggeforløb' },
           component: () => import('@/views/customer/ProcessView.vue'),
         },
         {
           path: 'process/:id',
           name: 'customer-process-details',
+          meta: { title: 'Byggeforløb Detaljer', layout: 'detail' },
           component: () => import('@/views/customer/ProcessDetailsView.vue'),
         },
         {
           path: 'chat-options',
           name: 'customer-chat-options',
+          meta: { title: 'Chat Muligheder' },
           component: () => import('@/views/customer/ChatOptionsView.vue'),
         },
         {
           path: 'chat',
           name: 'customer-chat',
+          meta: { title: 'Chat', layout: 'detail' },
           component: () => import('@/views/customer/ChatView.vue'),
         },
         {
           path: 'faq',
           name: 'customer-faq',
+          meta: { title: 'FAQ', layout: 'detail' },
           component: () => import('@/views/customer/FAQView.vue'),
         },
         {
           path: 'documents',
           name: 'customer-documents',
+          meta: { title: 'Dokumenter' },
           component: () => import('@/views/customer/DocumentsView.vue'),
         },
       ]
@@ -59,46 +64,55 @@ const router = createRouter({
         {
           path: '',
           name: 'manager-dashboard',
+          meta: { title: 'Manager Dashboard' },
           component: () => import('@/views/manager/ManagerDashboardView.vue')
         },
         {
           path: 'projects',
           name: 'manager-projects',
+          meta: { title: 'Projekter' },
           component: () => import('@/views/manager/ProjectsView.vue')
         },
         {
           path: 'project/:id',
           name: 'manager-project-details',
+          meta: { title: 'Projekt Detaljer', layout: 'detail'},
           component: () => import('@/views/manager/ProjectDetailView.vue')
         },
         {
           path: 'process',
           name: 'manager-process',
+          meta: { title: 'Byggeforløb' },
           component: () => import('@/views/manager/ProcessView.vue')
         },
         {
           path: 'chat',
           name: 'manager-chat',
+          meta: { title: 'Chat', layout: 'detai' },
           component: () => import('@/views/manager/ChatView.vue')
         },
         {
           path: 'chat-options',
           name: 'manager-chat-options',
+          meta: { title: 'Chat Muligheder' },
           component: () => import('@/views/manager/ChatOptionsView.vue')
         },
         {
           path: 'chat-list',
           name: 'manager-chat-list',
+          meta: { title: 'Igangværende Chats', layout: 'detail' },
           component: () => import('@/views/manager/ChatListView.vue')
         },
         {
           path: 'create-project',
           name: 'manager-create-project',
+          meta: { title: 'Opret Projekt' },
           component: () => import('@/views/manager/CreateProjectView.vue')
         },
         {
           path: 'documents',
           name: 'manager-documents',
+          meta: { title: 'Dokumenter' },
           component: () => import('@/views/manager/DocumentsView.vue')
         },
       ]
