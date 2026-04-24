1ee594eb5ff8787f0f59c29799ef1a7226b34d83
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-24 16:56:32 +0200

    feat: display project name on customer dashboard

--- src/views/customer/DashboardView.vue
+++ src/views/customer/DashboardView.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useProjectStore } from '@/stores/project';
+
+const store = useProjectStore();
+
+onMounted(() => {
+  store.fetchProjects();
+});
 </script>
 
 <template>
+  <h2>{{ store.projects[0]?.name }}</h2>
   <div class="dashboard">
     <RouterLink to="/customer/process" class="dashboard__card">
       Nuværende fase
