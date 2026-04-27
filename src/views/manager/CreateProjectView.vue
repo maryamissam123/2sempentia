816de6418a5f2a6a993a1d6b301841d41562c99e
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-27 14:48:50 +0200

    feat: add project info fields to create project form

--- src/views/manager/CreateProjectView.vue
+++ src/views/manager/CreateProjectView.vue
@@ -5,6 +5,10 @@ import { usePhaseStore } from '@/stores/phase';
 const store = usePhaseStore();
 const selectedPhases = ref([]);
 
+const projectNumber = ref('');
+const projectName = ref('');
+const projectAddress = ref('');
+
 onMounted(() => {
   store.fetchStandardPhases();
 });
@@ -21,10 +25,23 @@ function togglePhase(phase) {
 function isSelected(phase) {
 	return selectedPhases.value.some(p => p.id === phase.id);
 };
+
+function createProject() {
+	console.log({
+		projectNumber: projectNumber.value,
+		projectName: projectName.value,
+		projectAddress: projectAddress.value
+	})
+};
 </script>
 
 <template>
   <h1>Manager Create Project</h1>
+	<input v-model="projectNumber" placeholder="Projektnummer">
+	<input v-model="projectName" placeholder="Projektnavn">
+	<input v-model="projectAddress" placeholder="Projekt Adresse">
+
+	<h3>Vælg faser</h3>
 	<div 
 		v-for="phase in store.standardPhases"
 		:key="phase.id"
@@ -32,7 +49,8 @@ function isSelected(phase) {
 		:style="{ background: isSelected(phase) ? 'lightgreen' : 'white' }"
 		@click="togglePhase(phase)"
 	>
-	{{ phase.name }}
-
+		{{ phase.name }}
 	</div>
+
+	<button @click="createProject">Opret Projekt</button>
 </template>
