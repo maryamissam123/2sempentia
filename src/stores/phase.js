29900cdc2700ab914a735490bcc4cec72d7717b6
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-27 14:16:56 +0200

    feat: fetch and display standard phases on create project view

---
+++ src/stores/phase.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/firebase';
+
+export const usePhaseStore = defineStore('phase', () => {
+    const standardPhases = ref([]);
+
+    async function fetchStandardPhases() {
+        const snap = await getDocs(collection(db, 'standardPhases'));
+        standardPhases.value = snap.docs.map(d => ({ id: d.id, ...d.data() }));
+    }
+
+return { standardPhases, fetchStandardPhases }
+});
+
