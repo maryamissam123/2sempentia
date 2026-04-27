8e4d861c850ec580856ded10813dcc68bf43cfae
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-27 14:36:31 +0200

    feat: add toggle phases list to create project view

--- src/views/manager/CreateProjectView.vue
+++ src/views/manager/CreateProjectView.vue
@@ -1,15 +1,38 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { usePhaseStore } from '@/stores/phase';
 
 const store = usePhaseStore();
+const selectedPhases = ref([]);
 
 onMounted(() => {
   store.fetchStandardPhases();
 });
+
+function togglePhase(phase) {
+	const index = selectedPhases.value.findIndex(p => p.id === phase.id);
+	if(index === -1) {
+		selectedPhases.value.push(phase);
+	} else {
+		selectedPhases.value.splice(index, 1);
+	}
+};
+
+function isSelected(phase) {
+	return selectedPhases.value.some(p => p.id === phase.id);
+};
 </script>
 
 <template>
   <h1>Manager Create Project</h1>
-	<pre> {{ store.standardPhases }}</pre>
+	<div 
+		v-for="phase in store.standardPhases"
+		:key="phase.id"
+		class="test"
+		:style="{ background: isSelected(phase) ? 'lightgreen' : 'white' }"
+		@click="togglePhase(phase)"
+	>
+	{{ phase.name }}
+
+	</div>
 </template>
