db5b6a710f319bfb4b4c27da8ecb0ccf543ae25b
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-04-28 13:44:21 +0200

    feat: display phase comments on customer process details view

--- src/views/customer/ProcessDetailsView.vue
+++ src/views/customer/ProcessDetailsView.vue
@@ -13,13 +13,22 @@ onMounted(async() => {
 	const projectId = projectStore.projects[0]?.id;
 	if (projectId) {
 		phaseStore.fetchPhase(projectId, route.params.id);
-	}
+    phaseStore.fetchComments(projectId, route.params.id);
+	};
 });
 </script>
 
 <template>
-      <div>
+  <div>
     <h2>{{ phaseStore.phase?.name }}</h2>
     <p>Status: {{ phaseStore.phase?.completed ? 'færdig' : 'ikke færdig' }}</p>
   </div>
+
+  <h3>Kommentar fra byggelederen</h3>
+  <ul>
+    <li v-for="comment in phaseStore.comments" :key="comment.id">
+      {{ comment.text }}
+    </li>
+  </ul>
+
 </template>
