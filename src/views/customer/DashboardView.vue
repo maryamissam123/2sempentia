9132de1d36ade73f1a678eedd4500cac81786206
maryamissam123 <maryamissam123@gmail.com>
2026-04-30 17:58:10 +0200

    build: add dashboard list and base for view

--- src/views/customer/DashboardView.vue
+++ src/views/customer/DashboardView.vue
@@ -3,10 +3,17 @@ import { onMounted } from 'vue';
 import { useProjectStore } from '@/stores/project';
 import { usePhaseStore } from '@/stores/phase';
 import BaseCard from '@/components/BaseCard.vue';
+import BaseList from '@/components/BaseList.vue';
 import ProgressBar from '@/components/ProgressBar.vue';
 
 const projectStore = useProjectStore();
 const phaseStore = usePhaseStore();
+
+const dashboardLinks = [
+  { label: 'SE BYGGEFORLØB', route: '/customer/process', icon: 'ChartIcon.svg' },
+  { label: 'CHAT', route: '/customer/chat-options', icon: 'ChatIcon.svg' },
+  { label: 'FILER', route: '/customer/documents', icon: 'FilesIcon.svg' }
+];
 
 onMounted(async () => {
   await projectStore.fetchProjects();
@@ -18,7 +25,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  
+  <div class="dashboard-page">
   <section class="dashboard-header">      
       <div class="dashboard-header__image-container">
         <img 
@@ -31,24 +38,46 @@ onMounted(async () => {
 
   <ProgressBar :value="phaseStore.progress" />
 
-  <div class="dashboard">
-    <RouterLink to="/customer/process" class="dashboard__card">
-      Nuværende fase
-    </RouterLink>
-    <RouterLink to="/customer/process" class="dashboard__card">
-      Næste fase
-    </RouterLink>
-    <RouterLink to="/customer/process" class="dashboard__card">
-      Se byggeforløb
-    </RouterLink>
-    <RouterLink to="/customer/chat-options" class="dashboard__card">
-      Chat
-    </RouterLink>
-    <RouterLink to="/customer/documents" class="dashboard__card">
-      Dokumenter
-    </RouterLink>
-  </div>
-  base card test:
-  <BaseCard />
+  <div class="dashboard-content">
+      <!-- Nuværende og Næste fase -->
+      <div class="dashboard-grid">
+        <RouterLink to="/customer/process" class="dashboard-card">
+          <BaseCard 
+            stack 
+            title="NUVÆRENDE FASE" 
+            subtitle="Vinduersmontering"
+          >
+          <template #icon>
+              <img src="@/assets/icons/Window.png" alt="Vinduer" />
+            </template>
+          </BaseCard>
+        </RouterLink>
 
+        <RouterLink to="/customer/process" class="dashboard-card">
+          <BaseCard 
+            stack 
+            title="NÆSTE FASE" 
+            subtitle="Indvendig opbygning"
+          >
+            <template #icon>
+              <img src="@/assets/icons/Indoor.png" alt="Indretning" />
+            </template>
+          </BaseCard>
+        </RouterLink>
+      </div>
+
+      <!-- Generiske Liste -->
+      <BaseList :items="dashboardLinks" class="dashboard-list">
+        <template #item="{ item }">
+          <RouterLink :to="item.route" class="no-underline">
+            <BaseCard :title="item.label">
+              <template #icon>
+                <img :src="`/src/assets/icons/${item.icon}`" :alt="item.label" />
+              </template>
+            </BaseCard>
+          </RouterLink>
+        </template>
+      </BaseList>
+    </div>
+  </div>
 </template>
