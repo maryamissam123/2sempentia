5e207fb005a608a099d5c48efb16018231030a3f
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-05-04 13:52:23 +0200

    feat: add row and stack variants to BaseCard

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -8,13 +8,12 @@ defineProps({
 
 <template>
   <div class="base-card" :class="{ 'base-card--stack': stack }">
-    <h3 v-if="title" class="base-card__title">{{ title }}</h3>
-
     <div v-if="$slots.icon" class="base-card__icon">
       <slot name="icon" />
     </div>
 
     <div class="base-card__content">
+      <h3 v-if="title" class="base-card__title">{{ title }}</h3>
       <p v-if="subtitle" class="base-card__subtitle">{{ subtitle }}</p>
       <slot />
     </div>
