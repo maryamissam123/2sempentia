3c83d179c5c8b86ec8716ad02c11d925edba4fa4
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-05-04 15:39:38 +0200

    feat: use PhaseIcon component for dynamic icons in dashboard

--- src/components/PhaseIcon.vue
+++ src/components/PhaseIcon.vue
@@ -1,6 +1,28 @@
 <script setup>
+defineProps({
+  name: String,
+})
+
+const iconMap = {
+  fundament: 'Foundation.png',
+  vægge: 'Wall.png',
+  tag: 'Roof.png',
+  vinduer: 'Window.png',
+  dør: 'Door.png',
+  interiør: 'Interior.png',
+  nøgle: 'Key.png',
+}
+
+function getIcon(name) {
+  if (!name) return ''
+  const n = name.toLowerCase()
+  for (const key in iconMap) {
+    if (n.includes(key)) return iconMap[key]
+  }
+  return ''
+}
 </script>
 
 <template>
-    <div></div> 
+  <img v-if="name" :src="`/src/assets/icons/${getIcon(name)}`" :alt="name" />
 </template>
