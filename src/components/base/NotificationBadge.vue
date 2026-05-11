ba13a82d69dc247d224106af9f1dc1f67f87a0e9
maryamissam123 <maryamissam123@gmail.com>
2026-05-11 20:24:19 +0200

    build: add notification components

---
+++ src/components/base/NotificationBadge.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  count: {
+    type: [Number, String],
+    default: 0
+  }
+});
+</script>
+
+<template>
+  <div v-if="count > 0" class="notification-badge">
+    {{ count }}
+  </div>
+</template>
