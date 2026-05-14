e39a059ff0d34fb85ed7136aa5c045c4689da26e
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-05-14 13:21:01 +0200

    feat: show customer name and project image in manager chat list

--- src/composables/useManagerProject.js
+++ src/composables/useManagerProject.js
@@ -1,5 +1,5 @@
 // src/composables/useManagerProject.js
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useProjectStore } from '@/stores/project';
 import { usePhaseStore } from '@/stores/phase';
@@ -12,14 +12,16 @@ export function useManagerProject() {
   const phaseStore = usePhaseStore();
   const chatStore = useChatStore();
 
+  const customers = ref({})
+
   // Henter projectId fra URL - da manager har flere projekter
   const projectId = computed(() => 
     route.params.projectId || route.params.id || null
   );
 
   // Henter liste af managers projekter
-  function loadProjects() {
-    projectStore.fetchProjects();
+  async function loadProjects() {
+    await projectStore.fetchProjects();
   };
 
   // Henter specifikt projekt og customerdata
@@ -31,20 +33,27 @@ export function useManagerProject() {
     };
   };
 
+  // Henter customers for alle projekter
+  async function loadCustomersForProjects() {
+    for (const project of projectStore.projects) {
+      if (project.customerId) {
+        await projectStore.fetchCustomer(project.customerId);
+        customers.value[project.id] = projectStore.customer;
+      };
+    };
+  };
+
   // Henter alle faser for projektet
   function loadPhases() {
     if (projectId.value) phaseStore.fetchPhases(projectId.value);
   };
 
   // Henter specifik fase
-function loadPhase() {
-  console.log('route.params:', route.params);
-  console.log('projectId.value:', projectId.value);
-  console.log('phaseId:', route.params.id);
-  if (projectId.value && route.params.id) {
-    phaseStore.fetchPhase(projectId.value, route.params.id);
+  function loadPhase() {
+    if (projectId.value && route.params.id) {
+      phaseStore.fetchPhase(projectId.value, route.params.id);
+    };
   };
-};
 
   // Henter beskeder for projektet
   function loadMessages() {
@@ -56,10 +65,12 @@ function loadPhase() {
     projectStore,
     phaseStore,
     chatStore,
+    customers,
     loadProjects,
     loadProject,
     loadPhases,
     loadPhase,
     loadMessages,
+    loadCustomersForProjects,
   };
 };
--- src/views/manager/ChatListView.vue
+++ src/views/manager/ChatListView.vue
@@ -1,22 +1,23 @@
 <script setup>
 import { onMounted } from 'vue';
-import { useProjectStore } from '@/stores/project';
+import { useManagerProject } from '@/composables/useManagerProject';
 
-const projectStore = useProjectStore();
+const { projectStore, customers, loadProjects, loadCustomersForProjects } = useManagerProject();
 
-onMounted(() => {
-  projectStore.fetchProjects();
+onMounted(async () => {
+  await loadProjects();
+  await loadCustomersForProjects();
 });
 </script>
 
 <template>
-  <h1>Manager Chat Overview</h1>
-
     <RouterLink
     v-for="project in projectStore.projects"
     :key="project.id"
     :to="{ name: 'manager-chat', params: { projectId: project.id } }"
-  >
+  > 
+    <img v-if="project.imageUrl" :src="project.imageUrl" />
+    <strong>{{ customers[project.id]?.name }}</strong>
     {{ project.name }}
   </RouterLink>
 </template>
