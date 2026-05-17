a378e6ccb218621ccdbae902a855e78d5a98dc28
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-05-17 15:36:32 +0200

    feat: customer can view project documents

--- src/views/customer/DocumentsView.vue
+++ src/views/customer/DocumentsView.vue
@@ -1,6 +1,20 @@
 <script setup>
+import { watch } from 'vue';
+import { useCustomerProject } from '@/composables/useCustomerProject';
+import { useDocumentStore } from '@/stores/document';
+
+const { projectId } = useCustomerProject();
+const documentStore = useDocumentStore();
+
+watch(projectId, (id) => {
+  if (id) documentStore.fetchDocuments(id);
+}, { immediate: true });
 </script>
 
 <template>
-    <h1>Documents</h1>
+  <ul>
+    <li v-for="doc in documentStore.documents" :key="doc.id">
+      <a :href="doc.url" target="_blank">{{ doc.name }}</a>
+    </li>
+  </ul>
 </template>
